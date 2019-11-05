336cea025444aa1a76870bf91ced5ff09201c838
Jnglbt <anton.chepov@gmail.com>
2019-11-05 23:21:39 +0100

    Add main screen page

---
+++ frontend/src/pages/MainScreen.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Link, NavLink, Redirect} from 'react-router-dom';
+
+class MainScreen extends Component{
+    render() {
+        return <h1>Hello, world!</h1>
+    }
+}
+
+export default MainScreen
