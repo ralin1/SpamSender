963d33167761c7995bce13544ec04ccacbf786cb
Oleksandr <oleksandrfilippov2@gmail.com>
2019-11-06 11:30:20 +0100

    fix screens

--- frontend/src/pages/MainScreen.js
+++ frontend/src/pages/MainScreen.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
 import {Link, NavLink, Redirect} from 'react-router-dom';
+import Logo from "../logo1.png";
 
 class MainScreen extends Component{
     render() {
-        return <h1>Hello, world!</h1>
+        return <div className="App">
+                <div className="App__Aside_main">
+
+                </div>
+                <div className="App__Form">
+
+                </div>
+            </div>
     }
 }
 
